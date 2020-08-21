281d7fee3d9e1ef06eaa1a20d68473948718252d
ygcho <sorisori01@naver.com>
2020-08-21 10:28:54 +0900

    React Router #2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
 import Router from "./Components/Router";
-
+import Header from "./Components/Header";
 const App = () => (
     <>
+        <Header/>
         <Router />
     </>
 );
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import Home from "../Routes/Home";
 import TV from "../Routes/TV";
 import Search from "../Routes/Search";
@@ -7,11 +7,12 @@ import Search from "../Routes/Search";
 export default () => {
     return (
         <Router>
-            <>
+            <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/tv" component={TV} />
                 <Route path="/search" component={Search} />
-            </>
+                <Redirect from="*" to="/" />
+            </Switch>
         </Router>
     );
 };
