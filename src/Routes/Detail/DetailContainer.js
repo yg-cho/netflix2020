9f98ff5e0cb6c7409b27ff65b07684cf77aeb13c
ygcho <sorisori01@naver.com>
2020-09-19 15:51:53 +0900

    Detail Process #2

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {useParams, useLocation} from "react-router-dom";
+import {tvAPI, moviesAPI} from "../../api"
 
-const DetailContainer = () => {
+const DetailContainer = ({ pathname }) => {
+
+    let {id} = useParams();
+    let location = useLocation();
+
+    const [item, setItem] = useState({
+        result : {},
+        resulterror : null,
+        loading : true
+    })
+
+    // 화면이 로딩될때 무조건 실행되는 기능
+    useEffect(() => {
+        // networking
+        getData()
+    }, [id])
+
+    const getData = async () => {
+        console.log("123");
+    }
+
+
+
     return (
         <div>
             <h1>
-                Detail
+                {id}
             </h1>
         </div>
     );
